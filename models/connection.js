a2e474534bd0a3a0d9edae89667beea2beb2ab75
Mehdi Hadjadj <mehdi.hadjadj@outlook.fr>
2022-07-28 23:22:55 +0200

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,18 +1,18 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var options = {
-    connectTimeoutMS: 5000,
-    useNewUrlParser: true,
-    useUnifiedTopology : true
-   }
-   mongoose.connect('mongodb+srv://lacapsule:AInUolfyadVEx8g1@cluster0.o6j2r.mongodb.net/blackboard?retryWrites=true&w=majority', 
-      options,         
-      function(err) {
-          if (err){
-            console.log(err);
-          } else {
-            console.log('Connection à la base de données réussi');
-          }
-       
-      }
-   );
+  connectTimeoutMS: 5000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(
+  "mongodb+srv://blackboard:KRsEro8HYOIzopFX@cluster.dq2na.mongodb.net/blackboard",
+  options,
+  function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connection à la base de données réussi");
+    }
+  }
+);
